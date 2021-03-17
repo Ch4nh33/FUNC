42a4d124a6791aa14753252cbbca5116fb560839
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-03-18 08:13:46 +0900

    Create MySrtCpy.c

---
+++ MySrtCpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <string.h>
+
+char *strcpy(char *dest, const char *src);
+
+int main(void)
+{ 
+    char *stirng = "hello";
+    
+    strcpy(buf,stirng);
+    printf("%s",buf);
+    
+    return 0; 
+}
+
+char *strcpy(char *dest, const char *src)
+{
+    size_t i =0;
+    while(src[i] != '\0'){
+        dest[i] = src[i];
+        ++i;
+    }
+    dest[i] = '\0';
+    return dest;
+}    char buf[10] ={0, };
